5fa722e1b978d0870902876807dcd1edf004be8c
MatheusABC <matheuscantarutti@gmail.com>
2020-11-02 09:59:14 -0300

    criação da classe usuário

---
+++ ticketToThrive/src/main/java/com/mycompany/tickettothrive/models/Usuario.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.tickettothrive.models;
+
+import com.mycompany.tickettothrive.models.enums.EType;
+
+
+public class Usuario {
+    private String nome;
+    private String login;
+    private String password;
+    private EType userType;
+    
+    public Usuario(){
+
+    }
+}
+
