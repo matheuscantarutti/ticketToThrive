28bbc8f3cfa8dc55646ce45773cfadb41faa790e
MatheusABC <matheuscantarutti@gmail.com>
2020-11-21 11:56:04 -0300

    criação da classe ChamadoDao e método insert

--- ticketToThrive/src/main/java/DAO/ChamadoDao.java
+++ ticketToThrive/src/main/java/DAO/ChamadoDao.java
@@ -5,7 +5,7 @@
  */
 package DAO;
 
-import com.mycompany.tickettothrive.models.Usuario;
+import com.mycompany.tickettothrive.models.Chamado;
 import database.ConnectionDataBase;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,49 +15,52 @@ import java.sql.SQLException;
  *
  * @author mathe
  */
-public class UsuarioDao implements BasicDao<Usuario, Integer> {
+public class ChamadoDao implements BasicDao<Chamado, Integer> {
 
     @Override
-    public void insert(Usuario t) throws ClassNotFoundException, SQLException {
-        Connection c = ConnectionDataBase.getConnection();
+    public void insert(Chamado t) throws ClassNotFoundException, SQLException {
         
+        Connection c = ConnectionDataBase.getConnection();
 
-        String sql = "INSERT INTO `ticket_to_thrive`.`usuario`\n"
-                + "`nome`,\n"
-                + "`login`,\n"
-                + "`senha`,\n"
-                + "`tipo`,\n"
-                + "`departamento`)\n"
+        String sql = "INSERT INTO `ticket_to_thrive`.`chamado`\n"
+                + "`id_usuario`,\n"
+                + "`prioridade`,\n"
+                + "`descricao`,\n"
+                + "`tratamento`,\n"
+                + "`nascimento`,\n"
+                + "`obito`)\n"
                 + "VALUES\n"
                 + "?,\n"
                 + "?,\n"
+                + "}?,\n"
+                + "}?,\n"
                 + "?,\n"
                 + "?,\n"
                 + "?;";
         
         PreparedStatement pst = c.prepareStatement(sql);
-        pst.setString(1, t.getNome());
-        pst.setString(2, t.getLogin());
-        pst.setString(3, t.getPassword());
-        pst.setObject(4, t.getUserType());
-        pst.setString(5, t.getDepartamento());
+        pst.setObject(1, t.getCriador().getId());
+        pst.setObject(2, t.getPrioridade());
+        pst.setString(3, t.getDescricao());
+        pst.setString(4, t.getTratamento());
+        pst.setObject(5, t.getNascimento());
+        pst.setObject(5, t.getObito());
         
         pst.executeUpdate();
-        
     }
 
     @Override
-    public void alter(Usuario t) throws ClassNotFoundException, SQLException {
+    public void alter(Chamado t) throws ClassNotFoundException, SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void delete(Usuario t) throws ClassNotFoundException, SQLException {
+    public void delete(Chamado t) throws ClassNotFoundException, SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Usuario searchPerId(Integer id) throws ClassNotFoundException, SQLException {
+    public Chamado searchPerId(Integer id) throws ClassNotFoundException, SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -66,7 +69,4 @@ public class UsuarioDao implements BasicDao<Usuario, Integer> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
-
-    
 }
--- ticketToThrive/src/main/java/com/mycompany/tickettothrive/models/Chamado.java
+++ ticketToThrive/src/main/java/com/mycompany/tickettothrive/models/Chamado.java
@@ -6,7 +6,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-class Chamado {
+public class Chamado {
     private int id;
     private String descricao;
     private String tratamento;
